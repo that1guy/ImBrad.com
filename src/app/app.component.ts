92563a60b7d4df236ea6e35822db972b2f28020f
Brad Davis <brad@transparentproperties.com>
2017-08-21 22:20:14 -0700

    checking continuous deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -140,7 +140,7 @@ export class AppComponent {
       'position': 'fixed',
       'width': '100%',
       'height': '100%',
-      'z-index': 1,
+      'z-index': 2,
       'top': 0,
       'left': 0,
       'right': 0,
